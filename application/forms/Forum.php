cb2b6a7a00df281c904fbdf674fb410a0f93b016
ahmedalaasoliman <ahmed.alaasoliman@gmail.com>
2016-04-19 00:08:40 +0200

    hi

---
+++ application/forms/Forum.php
@@ -0,0 +1,13 @@
+<?php
+
+class Application_Form_Forum extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    }
+
+
+}
+
---
+++ application/models/DbTable/ForumModel.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_ForumModel extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'forum';
+
+
+}
+
