f723298db1c8a681d8a4335886d98d599ee73e07
amagdy529 <amagdy529@gmail.com>
2016-04-21 20:04:16 +0200

    login &header links updated

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -11,6 +11,30 @@ class IndexController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
+        //echo "inside index controller action !!";
+
+        $auth = Zend_Auth::getInstance();
+		$storage = $auth->getStorage();
+		$storage->read();
+
+		//var_dump($storage->read());
+		
+		$username= $storage->read()->username;
+		$id=$storage->read()->id;
+		$email=$storage->read()->email;
+		$gender=$storage->read()->gender;
+		$country=$storage->read()->country;
+		$signature=$storage->read()->signature;
+		$image=$storage->read()->image;
+
+		$this->view->username=$username;
+		$this->view->id=$id;
+		$this->view->email=$email;
+		$this->view->image=$image;
+		$this->view->signature=$signature;
+		$this->view->country=$country;
+		//die();
+		$this->render();
     }
 
 
