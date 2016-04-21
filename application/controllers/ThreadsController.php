46c51108ed8fcfff93e22becfdcc95e091c4af52
amagdy529 <amagdy529@gmail.com>
2016-04-21 22:33:51 +0200

    add thread

--- application/controllers/ThreadsController.php
+++ application/controllers/ThreadsController.php
@@ -3,9 +3,13 @@
 class ThreadsController extends Zend_Controller_Action
 {
 
+    private $model = null;
+    
     public function init()
     {
         /* Initialize action controller here */
+        $this->model = new Application_Model_DbTable_ThreadModel();
+        
     }
 
     public function indexAction()
@@ -16,6 +20,22 @@ class ThreadsController extends Zend_Controller_Action
     public function addAction()
     {
         // action body
+        
+        $data = $this->getRequest()->getParams();
+        $form = new Application_Form_Thread();
+        
+        if($this->getRequest()->isPost()){
+            
+            if($form->isValid($data)){
+            if ($this->model->addThread(1,$data))
+            $this->redirect('threads/index');
+            }   
+        }
+
+        //$this->view->flag = 1;
+        $this->view->form = $form;
+        $this->render('form');
+
     }
 
     public function editAction()
--- application/models/DbTable/ThreadModel.php
+++ application/models/DbTable/ThreadModel.php
@@ -5,6 +5,19 @@ class Application_Model_DbTable_ThreadModel extends Zend_Db_Table_Abstract
 
     protected $_name = 'thread';
 
+    function addThread($userId,$data){
+		
+		$row = $this->createRow();
+		$row->title=$data['title']; 
+		$row->body =$data['body'];
+		$row->is_sticky=$data['is_sticky'];
+		$row->is_opened=$data['is_opened'];
+		$row->forum_id=$data['forumId'];
+		$row->user_id=$userId;
+		$row->category_id=$data['categoryId'];
 
+		return $row->save();
+	
+	}
 }
 
