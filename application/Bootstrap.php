3997ae41d358f9a0646f46fa0cd39becdb14ceb8
mohamed-ashour <e.mohamed77@gmail.com>
2016-04-23 02:03:34 +0200

    fix project

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,20 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+	protected function _initPlaceholders()
+	{
+		//
+		// $this->bootstrap('frontcontroller');
+		// $controller = Zend_Controller_Front::getInstance();
+		// $controller->setBaseUrl('/zend_pojects/ZendForum/public/');
 
+		function _initSession(){
+			Zend_Session::start();
+			$session = new
+			Zend_Session_Namespace( 'Zend_Auth' );
+			$session->setExpirationSeconds( 1800 );
+		}
+
+	}
 
 }
-
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,14 +5,37 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $this->view->hi = "hi";
     }
 
     public function indexAction()
     {
         // action body
+        //echo "inside index controller action !!";
+
+        $auth = Zend_Auth::getInstance();
+		$storage = $auth->getStorage();
+		$storage->read();
+
+		//var_dump($storage->read());
+
+		$username= $storage->read()->username;
+		$id=$storage->read()->id;
+		$email=$storage->read()->email;
+		$gender=$storage->read()->gender;
+		$country=$storage->read()->country;
+		$signature=$storage->read()->signature;
+		$image=$storage->read()->image;
+
+		$this->view->username=$username;
+		$this->view->id=$id;
+		$this->view->email=$email;
+		$this->view->image=$image;
+		$this->view->signature=$signature;
+		$this->view->country=$country;
+		//die();
+		$this->render();
     }
 
 
 }
-
