a559d7cab10b3cdc781b48673187abaad33e0f3d
amagdy529 <amagdy529@gmail.com>
2016-04-23 04:17:31 +0200

    index of home controller but still in the begining still missing some funtciotns

--- application/controllers/HomeController.php
+++ application/controllers/HomeController.php
@@ -3,14 +3,27 @@
 class HomeController extends Zend_Controller_Action
 {
 
+	public static $ForumsModel;
+    public static $CategoryModel;
+    public static $UserModel ;
+    public static $ThreadModel ;
     public function init()
     {
         /* Initialize action controller here */
+        $this->ThreadModel = new Application_Model_DbTable_ThreadModel();
+        $this->ForumsModel = new Application_Model_DbTable_ForumModel();
+        $this->CategoryModel = new Application_Model_DbTable_CategoryModel();
+        $this->UserModel = new Application_Model_DbTable_UserModel();
+
     }
 
     public function indexAction()
     {
         // action body
+        $this->view->threads = $this->ThreadModel->listThreads();
+        $this->view->categories=$this->CategoryModel->selectAllCategory();
+        $this->view->forums=$this->ForumsModel->selectAllForum();
+        $this->view->countForums=$this->ForumsModel->countCategoryForum(2);
     }
 
 
